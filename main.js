fd5fd41bf3de846ae345d035b35e89c052bfdd91
plaraje <61209614+plaraje@users.noreply.github.com>
2022-05-25 21:39:29 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    let nav = document.querySelector('.nav-bar');
+    let togg = document.querySelector('.togg-nav');
+    nav.classList.toggle('active');
+    togg.classList.toggle('active');
+}
